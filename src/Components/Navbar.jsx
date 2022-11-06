a97e81cfab25c26b802861f07601a0b82ac1e5f9
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-11-06 11:23:48 +0100

    Fixed mobile navigation issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Navbar() {
   const [toggleNavbar, setToggleNavbar] = useState(false);
-  const navBarstyles = {
-    navBarHidden: "hidden w-full md:block md:w-auto",
-    navBarDisplayed: "display w-full md:block md:w-auto"
-  }
-  const [navBarStyle, setNavBarStyle] = useState("hidden w-full md:block md:w-auto")
-
+  const location = useLocation();
   function handleBtnClick(e) {
     setToggleNavbar(!toggleNavbar);
     e.preventDefault();
-    toggleNavbar ? setNavBarStyle(navBarstyles.navBarDisplayed) : setNavBarStyle(navBarstyles.navBarHidden);
   }
+
+  useEffect(()=> {
+    setToggleNavbar(false)
+  }, [location])
+
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="flex flex-wrap justify-between items-center px-4 mx-auto">
@@ -47,7 +46,7 @@ function Navbar() {
             ></path>
           </svg>
         </button>
-        <div className={navBarStyle} id="navbar-default">
+        <div className={`w-full md:block md:w-auto ${toggleNavbar ? 'display' : 'hidden'}`} id="navbar-default">
           <ul className="flex flex-col items-end p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <NavLink
